925c52fb833faa77a06d5c4f232686e3203f93b2
Asanka Thennakoon <asankabandara18879@gmail.com>
2021-09-14 13:37:58 +0530

    add selection ability

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -18,11 +18,28 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _picksedLocation;
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _picksedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: <Widget>[
+          if (widget.isSelecting)
+            IconButton(
+                onPressed: _picksedLocation == null
+                    ? null
+                    : () {
+                        Navigator.of(context).pop(_picksedLocation);
+                      },
+                icon: Icon(Icons.check))
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -30,6 +47,20 @@ class _MapScreenState extends State<MapScreen> {
               widget.initialLoaction.longitude),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _picksedLocation == null
+            ? {
+                Marker(
+                  markerId: MarkerId('I'),
+                  position: const LatLng(7.0817935, 80.4700367),
+                ),
+              }
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _picksedLocation as LatLng,
+                ),
+              },
       ),
     );
   }
